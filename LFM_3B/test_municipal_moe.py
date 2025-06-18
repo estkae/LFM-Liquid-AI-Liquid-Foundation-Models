d3126902b8265b8bbcc0b5a310cecc5f0738dfd4
root <root@locaraai>
2025-06-18 17:30:27 +0200

    Fix MunicipalMoE test: add num_attention_heads to config
    - Added num_attention_heads=4 to MunicipalMoEConfig in test_expert_routing
    - Ensures hidden_size (128) is divisible by num_attention_heads (4)
    - Fixes AssertionError in PyTorch MultiheadAttention module

--- LFM_3B/test_municipal_moe.py
+++ LFM_3B/test_municipal_moe.py
@@ -50,6 +50,7 @@ def test_expert_routing():
         vocab_size=1000,
         hidden_size=128,
         num_hidden_layers=1,
+        num_attention_heads=4,  # Must divide hidden_size evenly
         num_experts=8,
         num_experts_per_tok=2
     )
